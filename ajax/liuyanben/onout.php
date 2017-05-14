b2b32dec9d4df9ebfe7168e2ec56b759d8dfe67a
xck438200 <1377425199@qq.com>
2017-05-14 20:48:34 +0800

    0514  go

---
+++ ajax/liuyanben/onout.php
@@ -0,0 +1,11 @@
+<?php
+    require_once("lianjiesql.php");
+    $sql = 'select title, content from message order by id asc';
+    $select = $dbh->query($sql);
+    $arr = [];
+    foreach ($select as $key => $value) {
+//        $time = date("Y年m月d h:i:s", $value['created_time']);
+        $arr['message'][$key] = $value;
+//        $arr['message'][$key]['created_time'] = $time;
+    }
+    echo json_encode($arr);
