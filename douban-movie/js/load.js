a591d315496c79c43533d6d63698a6ee1a648305
xck438200 <1377425199@qq.com>
2017-06-23 21:03:50 +0800

    0623

---
+++ douban-movie/js/load.js
@@ -0,0 +1,30 @@
+var itemsPerload = 8;
+var items = $(".items");
+// var page =1;
+var templeteStr = $("#templete").html();
+var tempStr = _.template(templeteStr);
+    $.get({
+        url : "mobile.php",
+        dataType: "json",
+        success: function (data) {
+            // console.log(data);
+            var obj = data.subjects;
+            // console.log(obj);
+            // $.each(obj,function (index) {
+            //     var img = obj[index].images.large;
+            //     $(".item-poster").append(img);
+            // });
+            // var img = obj[0].images.large;
+            // console.log(img)
+            $.each(obj, function (index, value) {
+                var kao = tempStr(value);
+                var $domObj = $(kao);
+                console.log($domObj);
+                $(".items").append($domObj);
+            });
+        }
+    });
+    console.log(obj);
+// addItems(itemsPerLoad);
+
+// var num = obj
---
+++ douban-movie/mobile.php
@@ -0,0 +1,4 @@
+<?php
+
+	$url = "https://api.douban.com/v2/movie/in_theaters";
+	echo $data = file_get_contents($url);
