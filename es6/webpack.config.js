fbcbfe49f9d39e148a351f819b767c8eaebb6e46
xck438200 <1377425199@qq.com>
2017-07-04 09:32:18 +0800

    study es6 first day

---
+++ es6/webpack.config.js
@@ -0,0 +1,20 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+    context: __dirname,
+    // entry: './src/index.js',
+    entry: ['./src/index.js', './src/main.js'],
+    watch: true,
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'bundle.js',
+        publicPath: '/dist/'
+    },
+    module: {
+        loaders: [
+            { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+        ]
+    }
+
+}
