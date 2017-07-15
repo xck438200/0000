da94979835aea045508f6c28931e6fe7c965ad41
xck438200 <1377425199@qq.com>
2017-07-15 20:57:34 +0800

    0715

---
+++ study/0330（js第1天）/js/Noname1.js
@@ -0,0 +1,24 @@
+/*
+document.write("haha");
+alert("屠龙宝刀点击就送");
+console.log(a);
+
+
+console.log(a);
+var a=16;
+
+console.log(a);
+var a=11;
+console.log(Infinity);
+console.log(0/0);
+console.log("7"-"5");
+console.log("hello"-"world");  
+*/
+var n1=1;n2=2;b1=true;b2=false;s1="1";s2="2";
+console.log(n2-s2);
+console.log(s2+n1);
+console.log(n1+s2);
+console.log(n1+s2-b1);
+console.log(s2-b1);
+console.log(s2+b1);
+console.log(b1+s2);
